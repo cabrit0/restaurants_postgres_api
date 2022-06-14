9af8d5b595397aae1ab3e3195cf4ba1927e6d2a0
cabrit0 <cabrit0o.dev@gmail.com>
2022-06-14 22:55:45 +0100

    cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 //const morgan = require("morgan");
+const cors = require("cors");
 const db = require("./db");
 const express = require("express");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
